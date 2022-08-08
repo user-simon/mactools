d530d288c8ccc5b2b302f1bcc538b5669f8b4eda
userexit <usersimon01@gmail.com>
2022-08-08 20:31:47 +0200

    Cleaned up fmt code

--- main.js
+++ main.js
@@ -1,50 +1,28 @@
-function fmtMAC(input, separator, group_size) {
-    let regex = new RegExp(`.{1,${group_size}}`, 'g');
-    return input.match(regex).join(separator)
+function onUpdate(input) {
+    fmtDelegate(input)
 }
 
-function onMACUpdate(input) {
-    if (input.length > 0) {
-        if (document.getElementById("uppercase").checked) {
-            processMAC(input.toUpperCase())
+function fmtDelegate(input) {
+    let raw = input.replace(/[^0-9a-fA-F]/gi, '');
+    let groups = document.getElementsByClassName("fmtGroup");
+    
+    for (let i = 0; i < groups.length; i++) {
+        let group = groups[i];
+        let group_size = group.dataset.size;
+
+        if (raw.length > group_size) {
+            let children = group.children;
+            let regex = new RegExp(`.{1,${group_size}}`, 'g');
+    
+            groups[i].style.visibility = "visible";
+
+            for (let i = 0; i < children.length; i++) {
+                let child = children[i];
+                let separator = child.dataset.sep;
+                child.value = raw.match(regex).join(separator);
+            }
         } else {
-            processMAC(input.toLowerCase())
+            groups[i].style.visibility = "hidden";
         }
-    }
-}
-
-function hideGroup(group_size) {
-    document.getElementById(group_size).hidden = true;
-}
-
-function showFmtGroup(raw, group_size) {
-    let group = document.getElementById(group_size);
-    group.hidden = false;
-    let children = group.children;
-
-    for (let i = 0; i < children.length; i++) {
-        let child = children[i];
-        let separator = child.dataset.sep;
-        child.value = fmtMAC(raw, separator, group_size)
-    }
-}
-
-function processMAC(input) {
-    let raw = input.replace(/[^0-9a-fA-F]/gi, '')
-
-    if (raw.length >= 2) {
-        showFmtGroup(raw, 2)
-
-        if (raw.length >= 4) {
-            showFmtGroup(raw, 4)
-        }
-    } else {
-        hideGroup(2);
-        hideGroup(4);
-    }
-    if (raw.length % 4 === 0) {
-        
-    }
-    if (raw.length >= 6) {
     }
 }
